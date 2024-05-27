093211178af2abb9817af86159474f822fba4029
invfaker <dakun340@gmail.com>
2024-05-28 05:34:49 +0700

    fix: screen reponsive

--- frontend/src/pages/BantuanPage.jsx
+++ frontend/src/pages/BantuanPage.jsx
@@ -6,7 +6,7 @@ function BantuanPage() {
   return (
     <div>
       <Headline>Bantuan</Headline>
-      <div className="grid md:grid-cols-3 xl:grid-cols-5 gap-4">
+      <div className="grid md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         <CardBantuan />
         <CardBantuan />
         <CardBantuan />
