bba6bd251e2634b6255e2b1c99d289245ae37aa2
Hula-Hula <fachry150503@gmail.com>
2024-06-06 15:29:16 +0700

    update_navbar infomap on going

--- frontend/src/pages/InformationPage.jsx
+++ frontend/src/pages/InformationPage.jsx
@@ -142,7 +142,12 @@ function InformationPage() {
           </div>
         </div>
       </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+
+      </div>
+
       <Footer />
+
     </>
   );
 }
