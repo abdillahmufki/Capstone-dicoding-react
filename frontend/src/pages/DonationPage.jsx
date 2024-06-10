39d681d8b63414b13f3e966b075a5b01237d465c
invfaker <dakun340@gmail.com>
2024-06-10 10:41:41 +0700

    auth backend

--- frontend/src/pages/DonationPage.jsx
+++ frontend/src/pages/DonationPage.jsx
@@ -5,6 +5,7 @@ import Footer from "../Layouts/Footer";
 import PageHeader from "../components/PageHeader";
 import { client } from "../client";
 import { urlFor } from "../sanityImageUrl";
+import Loader from "../components/Loader";
 
 async function getBlogPosts() {
   const query = `*[_type == "post-donation"]`;
@@ -26,25 +27,33 @@ function DonationPage() {
   return (
     <>
       <Header />
-      <div className="bg-white dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-          <PageHeader
-            pageName="Donation Platform"
-            pageDescription="Make a difference by contributing to earthquake relief efforts. Our donation platform allows you to support communities affected by earthquakes, funding emergency response, recovery initiatives, and rebuilding projects to help those in need."
-          />
-          <div className="grid grid-cols-4 gap-4 mt-10">
-            {platformDonation.map((platform) => (
-              <CardBantuan
-                key={platform.name}
-                url={platform.link}
-                name={platform.name}
-                description={platform.description}
-                image={urlFor(platform.image).url()}
-              />
-            ))}
+      {!platformDonation ? (
+        <div className="bg-white dark:bg-gray-900">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl min-h-screen sm:py-16 lg:px-6 flex items-center justify-center">
+            <Loader />
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="bg-white dark:bg-gray-900">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <PageHeader
+              pageName="Donation Platform"
+              pageDescription="Make a difference by contributing to earthquake relief efforts. Our donation platform allows you to support communities affected by earthquakes, funding emergency response, recovery initiatives, and rebuilding projects to help those in need."
+            />
+            <div className="grid grid-cols-4 gap-4 mt-10">
+              {platformDonation.map((platform) => (
+                <CardBantuan
+                  key={platform.name}
+                  url={platform.link}
+                  name={platform.name}
+                  description={platform.description}
+                  image={urlFor(platform.image).url()}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
       <Footer />
     </>
   );
