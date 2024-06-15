0725f19101f7ecc7c07562d31d6c8786960feffa
invfaker <dakun340@gmail.com>
2024-06-15 09:01:53 +0700

    add avatar for login

--- frontend/src/Layouts/Header.jsx
+++ frontend/src/Layouts/Header.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Navbar, Dropdown } from "flowbite-react";
+import { Navbar, Dropdown, Avatar } from "flowbite-react";
 import { WiEarthquake } from "react-icons/wi";
 import { Link } from "react-router-dom";
 
@@ -28,6 +28,32 @@ function Header() {
         </Link>
         <Navbar.Toggle />
       </div>
+      {/* <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div> */}
       <Navbar.Collapse>
         <Navbar.Link
           as={Link}
