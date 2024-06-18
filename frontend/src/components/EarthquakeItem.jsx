fffeb517345b282917b643e9552742a461f76118
Hula-Hula <fachry150503@gmail.com>
2024-06-18 21:29:43 +0700

    update close link

--- frontend/src/components/EarthquakeItem.jsx
+++ frontend/src/components/EarthquakeItem.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function EarthquakeItem({ index, Wilayah, Magnitude, Jam, DateTime, pengirim }) {
+function EarthquakeItem({ index, Wilayah, Magnitude, Potensi, Jam, DateTime }) {
   const jam = new Date(DateTime).toLocaleTimeString("id-ID", {
     hour: "2-digit",
     minute: "2-digit",
   });
 
   return (
-    <Link
-      to={`/detailgempa/${index}`}
+    <div
       className="block max-w-screen-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 transform transition duration-500 ease-in-out hover:scale-95"
     >
       <h5 className="font-bold text-gray-900 dark:text-white">{Wilayah}</h5>
       <p className="text-gray-700 dark:text-gray-400">
-        Sumber Informasi : {pengirim}
+        Sumber Informasi : Pengguna
       </p>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         Tingkat gempa atau magnitude : {Magnitude}
@@ -30,7 +29,7 @@ function EarthquakeItem({ index, Wilayah, Magnitude, Jam, DateTime, pengirim }) 
           year: "numeric",
         })}
       </p>
-    </Link>
+    </div>
   );
 }
 
